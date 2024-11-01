358e82549f61f9c9a8b6c2e7fbd8c0b0dc80c29e
Kristhel Cordero <129348691+KristhelCordero@users.noreply.github.com>
2024-10-31 19:49:42 -0600

    Reapply "Merge branch 'main' into Kristhel's-Branch"
    This reverts commit e5032a75e16cca2947a7802e6d0142716e6a7eae.

--- frontend/src/NavBar/SideBar.jsx
+++ frontend/src/NavBar/SideBar.jsx
@@ -1,26 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './SideBar.css';
 import gradient from '../assets/lightblue_yellow_gradient.png';
 import back1 from '../assets/back1.png';
 import user from '../assets/user.png';
-import selected from "../assets/Pestanaseleccionada.png"
+import selected from "../assets/Pestanaseleccionada.png";
 import pill from '../assets/drugs1.png';
 
 const SideBar = () => {
-
     return (
         <div className="sidebar">
             <div className="sidebar-title">Panel de navegación </div>
             <img className="back-1" alt="Back" src={back1} />
-            <div className="sidebar-link">Inicio</div>
-            <div className="sidebar-link">Mis solicitudes</div>
-            <div className="selected-image-container">
-                <img className="selected-tab" alt="selected" src={selected} />
-            </div>
-            <div className="sidebar-link">Nueva solicitud</div>
-            <div className="sidebar-link">Mis puntos</div>
-            <div className="sidebar-link">Ayuda</div>
+            <Link to="/" className="sidebar-link" onClick={() => changeOption('Inicio')}>Inicio</Link>
+            <Link to="/MyRequests" className="sidebar-link" onClick={() => changeOption('Solicitudes')}>Mis solicitudes</Link>
+            <Link to="/NewRequest" className="sidebar-link" onClick={() => changeOption('Nueva')}>Nueva solicitud</Link>
+            <Link to="/MyPoints" className="sidebar-link" onClick={() => changeOption('Puntos')}>Mis puntos</Link>
+            <Link to="/Help" className="sidebar-link" onClick={() => changeOption('Ayuda')}>Ayuda</Link>
             <div className="sidebar-user-container">
                 <div className="user_image_container">
                     <img src={user} alt="Logo" id="user" />
