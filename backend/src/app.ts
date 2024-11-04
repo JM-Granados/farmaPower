2c09115431038cdd3822d4ec48edef7e64afb269
Jose <josemacr04@gmail.com>
2024-11-04 03:53:49 -0600

    Versión n+1

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -56,7 +56,7 @@ app.use(express.urlencoded({ extended: true }));
 
 //----------------------------------------------------------------------------------
 //Crea las rutas
-app.use(users)
+app.use('/api/users',users)
 app.use('/api/requests',requests)
 app.use('/api/elegiblemedications',elegibleMedications)
 app.use('/api/medications', medications)
