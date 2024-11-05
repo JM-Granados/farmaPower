61f0d047990432dc1b00ad1adb6e4467c99bf00e
Kristhel Cordero <129348691+KristhelCordero@users.noreply.github.com>
2024-11-04 19:00:08 -0600

    asd

--- backend/src/routes/requests.ts
+++ backend/src/routes/requests.ts
@@ -4,7 +4,7 @@ import * as requestCtrl from '../controllers/requests.controller';
 const router = Router();
 
 
-router.get('/:id', requestCtrl.getRequests);
+router.get('/x/:id', requestCtrl.getRequests);
 router.post('/', requestCtrl.createRequest);
 router.get('/rStatus', requestCtrl.getRequests_RStatus);
 router.get('/totalPointsByMedication/:clientId', requestCtrl.getPointsByMedication);
