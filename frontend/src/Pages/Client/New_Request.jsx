1e0c7fb23e54fc08a4a4833886aa932ad4ee6c74
MeliCarvajalCharpentier01 <melicarvajalcharpentier.code@gmail.com>
2024-11-05 19:43:23 -0600

    partial new request

--- frontend/src/Pages/Client/New_Request.jsx
+++ frontend/src/Pages/Client/New_Request.jsx
@@ -1,126 +1,166 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './New_Request.css';
 import SideBar from '../../NavBar/SideBar';
 import '../../NavBar/SideBar.css';
-import gradient from '../../assets/new_request.png'; // Corrected path
+import gradient from '../../assets/new_request.png';
 import folder from '../../assets/folder.png';
-
+import upload from '../../assets/upload.png';
 
 const NewRequest = () => {
-
-  //   const handleSubmit = async (e) => {
-  //     e.preventDefault();
-  //     setErrorMessage('');
-
-  //     const formData = new FormData();
-  //     formData.append('firstName', FirstName !== undefined ? FirstName : "");
-  //     formData.append('secondName', SecondName !== undefined ? SecondName : "");
-  //     formData.append('firstLastName', FirstLastName !== undefined ? FirstLastName : "");
-  //     formData.append('secondLastName', SecondLastName !== undefined ? SecondLastName : "");
-  //     formData.append('username', username !== undefined ? username : "");
-  //     formData.append('email', email !== undefined ? email : "");
-  //     formData.append('password', password !== undefined ? password : "");
-  //     formData.append('birthdate', birthdate !== undefined ? birthdate : "");
-  //     formData.append('avatar', avatar !== undefined ? avatar : "");
-
-  //     try {
-  //         const result = await axios.post('http://localhost:4000/api/users/', formData, {
-  //             headers: {
-  //                 'Content-Type': 'multipart/form-data'
-  //             }
-  //         });
-
-  //         console.log(result);
-  //         if (result.data.message === "User created") {
-
-  //             navigate('/')
-  //         } else {
-  //             setErrorMessage('Unexpected response from the server.');
-  //         }
-
-  //     } catch (err) {
-  //         setErrorMessage(err.response.data.error || 'An error ocurred while creating the user.')
-  //     }
-  // }
+  // Definir el estado para cada campo
+  const [purchaseDate, setPurchaseDate] = useState('');
+  const [invoiceNumber, setInvoiceNumber] = useState('');
+  const [pharmacy, setPharmacy] = useState('');
+  const [pharmacies, setPharmacies] = useState([]);
+  const [purchasedQuantity, setPurchasedQuantity] = useState('');
 
 
+  useEffect(() => {
+    const fetchPharmacies = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/api/pharmacies/get');
+        setPharmacies(response.data);
+      } catch (error) {
+        console.error("Error fetching pharmacies:", error);
+      }
+    };
+
+    fetchPharmacies();
+  }, []);
+
+
+
+  // Manejar el envío del formulario
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = {
+      purchaseDate,
+      invoiceNumber,
+      pharmacy,
+      purchasedQuantity,
+    };
+    console.log('Datos enviados:', formData);
+    // Aquí puedes hacer la lógica para enviar los datos a un servidor
+  };
 
   return (
     <div className="nueva-solicitud">
       <div className="row principal">
-        {/* Paritmos la pantalla en 2
-                Primer pedazo
-                Las proporciones se ajustan con porcentajes en el css o se puede -N donde N es la cantidad de pedazos */}
         <div className="div">
           <SideBar />
         </div>
 
-        {/* Segundo pedazo
-                Este pedazo se divide en 3. Esta el titulo, el filtro y las solicitudes */}
-        <div className="col div2">
-          {/* Pedazo del titulo */}
-          <div className="row div3">
-            <div className="div-gradient-header d-flex justify-content-start align-items-end" style={{ height: '100%' }}>
-              <img className='imagen' src={gradient} alt="Logo" id="gradient" />
+        <div className="col nrdiv2">
+          <div className="row nrdiv3">
+            <div className="nrdiv-gradient-header d-flex justify-content-start align-items-end" style={{ height: '100%' }}>
+              <img className='nrimagen' src={gradient} alt="Logo" id="gradient" />
             </div>
           </div>
 
-          {/* Pedazo de las solicitudes */}
-          <div className="row div5">
-            <div className="col div6 input-group-column">
-
-
+          <form onSubmit={handleSubmit} className="row nrdiv5">
+            <div className="col rndiv6 input-group-column">
               <div className="row mt-4 align-items-center">
-                <div className="col-md-3">
+                <div className="col-md-4">
                   <p className="form-label text-white">Fecha de compra</p>
                 </div>
                 <div className="col-md-6">
-                  <input type="date" className="form-control" />
+                  <input
+                    type="date"
+                    className="form-control"
+                    value={purchaseDate}
+                    onChange={(e) => setPurchaseDate(e.target.value)}
+                  />
                 </div>
               </div>
 
               <div className="row mt-4 align-items-center">
-                <div className="col-md-3">
+                <div className="col-md-4">
                   <p className="form-label text-white">Número de factura</p>
                 </div>
                 <div className="col-md-6">
-                  <input type="text" className="form-control" />
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={invoiceNumber}
+                    onChange={(e) => setInvoiceNumber(e.target.value)}
+                  />
                 </div>
               </div>
 
               <div className="row mt-4 align-items-center">
-                <div className="col-md-3">
+                <div className="col-md-4">
                   <p className="form-label text-white">Farmacia</p>
                 </div>
-                <div class="dropdown">
-                  <a class="mbtn mbtn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Dropdown link
-                  </a>
+                <div className="dropdown">
+                  <select
+                    className="form-control"
+                    value={pharmacy}
+                    onChange={(e) => setPharmacy(e.target.value)}
+                  >
+                    <option value="">Selecciona una opción</option> 
+                    {pharmacies.map((pharmacy) => (
+                      <option key={pharmacy._id} value={pharmacy._id}>
+                        {pharmacy.name}
+                      </option>
+                    ))}
+                  </select>
+                </div>
 
-                  <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                  </ul>
+              </div>
+
+              <div className="row mt-4 align-items-center">
+                <div className="col-md-4">
+                  <p className="form-label text-white">Cantidad comprada</p>
+                </div>
+                <div className="col-md-6">
+                  <input
+                    type="number"
+                    className="form-control"
+                    value={purchasedQuantity}
+                    onChange={(e) => setPurchasedQuantity(e.target.value)}
+                  />
                 </div>
               </div>
-
-
-
-
             </div>
 
-            <div className="col-3 div7">
-              <div className="div-folder d-flex justify-content-start align-items-end">
-                <img className='imagen-folder' src={folder} />
+            <div className="col-3 rndiv8">
+              <div className="col-3 rndiv7">
+                <div className="div-upload d-flex justify-content-center align-items-end">
+                  <img
+                    className='imagen-upload'
+                    src={folder}
+                    alt="Upload icon"
+                    onClick={() => document.getElementById('file-upload').click()}
+                    style={{ cursor: 'pointer' }}
+                  />
+                  <input
+                    id="file-upload"
+                    type="file"
+                    style={{ display: 'none' }}
+                    onChange={(e) => console.log(e.target.files[0])} // Aquí manejas el archivo seleccionado
+                  />
+                </div>
+
               </div>
+              <div className="div-upload d-flex justify-content-center align-items-end">
+                <img
+                  className="imagen-upload"
+                  src={upload}
+                  alt="Upload icon"
+                  onClick={handleSubmit}
+                  style={{ cursor: 'pointer' }}
+                />
+              </div>
+
+
             </div>
 
-          </div>
+          </form>
         </div>
       </div>
     </div>
   );
 };
-
 
 export default NewRequest;
