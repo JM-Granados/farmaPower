c912c0bf9a019e5c2704216e999d33594571f519
Kristhel Cordero <129348691+KristhelCordero@users.noreply.github.com>
2024-11-06 13:15:08 -0600

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -48,10 +48,10 @@ app.set('port', config.MONGO_PORT);
 // Utiliza morgan para saber explícitamente las url que se visitan
 app.use(morgan('dev'));
 // Utiliza cors para decirle que cualquier servidor del frontend pude pedir cosas
-// app.use(cors({
-//     origin: process.env.FRONTEND_URL
-// }));
-app.use(cors()); //documentar esto cuando termine pruebas locales
+app.use(cors({
+    origin: process.env.FRONTEND_URL
+}));
+// app.use(cors()); //documentar esto cuando termine pruebas locales
 
 // Utilizada para que se puedan entender las peticiones json como los post que vienen con datos
 app.use(express.json());
